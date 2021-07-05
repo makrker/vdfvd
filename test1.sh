a78b4e527ab3406145076fe17ab19b189a95a58c
makrker <86200419+makrker@users.noreply.github.com>
2021-07-05 13:01:46 +0700

    Update test1.sh

--- test1.sh
+++ test1.sh
@@ -1,26 +1,21 @@
 #!/bin/bash
-
-cmd=(dialog --separate-output --checklist "Select options:" 22 76 16)
-options=(1 "KDE" off    # any option can be set to default to "on"
-         2 "GNOME" off
-         3 "i have no clue lold" off
-         4 "Option 4" off)
-choices=$("${cmd[@]}" "${options[@]}" 2>&1 >/dev/tty)
-clear
-for choice in $choices
+PS3='Please enter your choice: '
+options=("Option 1" "Option 2" "Option 3" "Quit")
+select opt in "${options[@]}"
 do
-    case $choice in
-        1)
-            echo "First Option"
+    case $opt in
+        "Option 1")
+            echo "you chose choice 1"
             ;;
-        2)
-            echo "Second Option"
+        "Option 2")
+            echo "you chose choice 2"
             ;;
-        3)
-            echo "Third Option"
+        "Option 3")
+            echo "you chose choice $REPLY which is $opt"
             ;;
-        4)
-            echo "Fourth Option"
+        "Quit")
+            break
             ;;
+        *) echo "invalid option $REPLY";;
     esac
 done
