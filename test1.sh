8f25cc9e582677d9edfce1f7cdcb877f2ffb9709
John Doe <dalex11awoo@protonmail.com>
2021-07-06 12:07:32 +0700

    ss's

--- test1.sh
+++ test1.sh
@@ -15,8 +15,8 @@ function update_user
  echo "yihohi"
  case $option in
  1 ) update_username;  echo "yr eehehehe" ;;
- 2 ) update_password; echo ur sussye desktop  ;;
- 3 ) main_menu;  break ;;
+ 2 ) update_password; echo ur "sussye desktop"  ;;
+ 3 ) main_menu;  exit ;;
  4 ) break ;;
  * ) tput setf 3;echo "Please enter 1, 2 or 3";tput setf 3; 
  esac
@@ -24,27 +24,4 @@ function update_user
  done
 }
 
-function main_menu 
-{
-option=0
-until [ "$option" = "4" ]; do
-echo "  1.) Add user"
-echo "  2.) Remove user"
-echo "  3.) Update user"
-echo "  4.) Quit"
-
-echo -n "Enter choice: "
-read -r option
-echo ""
-case $option in
-    1 ) add_user ; ech0 "daugdadawda"  ;;
-    2 ) remove_user ; echo "ahdahlad" ;;
-    3 ) update_user ; echo "ahdadhdahwhdiedohood" ;;
-    4 ) exit;;
-    * ) tput setf 4;echo "Please enter 1, 2, 3, or 4";tput setf 4; 
-esac
-done
- }
-
-main_menu
 
