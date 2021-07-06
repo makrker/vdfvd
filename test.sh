ae8f8f9241de3b83ab41481cf2ab6ea2308b1ff9
John Doe <dalex11awoo@protonmail.com>
2021-07-06 13:14:18 +0700

    sdasdada

--- test.sh
+++ test.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
-type apt >/dev/null 2>&1 || { echo >&2 "I require foo but it's not installed.  Aborting."; exit 1; }
-
-sudo apt-get update
+date() {
+    if hash hyper 2>/dev/null; then
+        sudo apt-get update"$@"
+    else
+        echo "Could not lol" "$@"
+    fi
+}
