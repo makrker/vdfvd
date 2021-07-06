4024c535d76125e6197c9d446889668de37ca6e3
John Doe <dalex11awoo@protonmail.com>
2021-07-06 11:31:15 +0700

    sc

--- test1.sh
+++ test1.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
 
 echo "www.co"
-echo "sused "
-sudo wget https://github.com/JanDeDobbeleer/oh-my-posh/releases/latest/download/posh-linux-amd64 -O /usr/local/bin/oh-my-posh
-sudo chmod +x /usr/local/bin/oh-my-posh
-echo "jk lol how to sussed "
+
 
 
 function update_user 
