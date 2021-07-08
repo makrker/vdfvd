0d91f8f8afac9fed98c72a5fb49b616bdeae0879
John Doe <dalex11awoo@protonmail.com>
2021-07-09 00:38:10 +0700

    jeje

--- start.sh
+++ start.sh
@@ -1,25 +1,38 @@
 #!/bin/bash 
 
-
-if [[ $(command -v brew) == "" ]]; then
+if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+   if [[ $(command -v brew) == "" ]]; then
     echo "You have not installed Homebrew "
     
- sudo apt install wget -y
- cd || exit #
-wget https://raw.githubusercontent.com/makrker/vdfvd/main/main.sh
+     sudo apt install wget -y
+      cd || exit #
+       wget https://raw.githubusercontent.com/makrker/vdfvd/main/main.sh
+        chmod +x main.sh
 
-chmod +x main.sh
+          ./main.sh
+       else
+         echo "You have installed Homebrew"
+           sudo apt install wget -y
+            cd /home/"$USER" || exit 
+            wget https://raw.githubusercontent.com/makrker/vdfvd/main/side.sh
 
-./main.sh
-else
-    echo "You have installed Homebrew"
+             chmod +x side.sh
+
+              ./side.sh
+
+
+    fi
+fi
+
+if [[ "$OSTYPE" == "darwin"* ]]; then
+     
+      cd || exit #
+       wget https://raw.githubusercontent.com/makrker/vdfvd/main/macos.sh
+        chmod +x macos.sh
+
+          ./macos.sh
        
- sudo apt install wget -y
- cd /home/"$USER" || exit 
-wget https://raw.githubusercontent.com/makrker/vdfvd/main/side.sh
 
-chmod +x side.sh
 
-./side.sh
-
+    
 fi
