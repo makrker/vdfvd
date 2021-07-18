6d85c6e214293895649ca9dcc5c8b8ab80581747
makrker <86200419+makrker@users.noreply.github.com>
2021-07-18 16:20:03 +0000

    Fix Flatpak and added sections
    fix flatpak not installing on debian and added more apps and added oh my posh downloads

--- main.sh
+++ main.sh
@@ -9,11 +9,29 @@ now=$(date +"%r")
 
 echo -e "\033[33;34m Current time : $now"
 sleep 1s
-flatpak remote-add --user --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+
+
+sudo apt update -y;sudo apt upgrade -y
+
+
+sudo apt install flatpak -y
+
+sudo apt update --fix-missing -y
+sudo apt install -f -y
+sudo apt install make -y 
+sudo apt install cmake -y 
+sudo apt autoremove -y
+sudo apt install perl -y
+sudo apt install figlet -y 
+sudo apt install keepassxc -y 
+
+
+
+flatpak remote-add --user --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo 
 sudo apt install git curl gcc  -y
  hostnamectl set-hostname xzxs --static
 sudo apt install wget make  -y
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"< /dev/null
 
 
 
@@ -25,55 +43,43 @@ brew upgrade
 brew install gcc 
  
 
-sudo apt --fix-broken install -y 
-echo "OPEN HYPER NOW "
 
-sleep 10s 
-sudo apt isntall figlet -y 
-sudo apt update -y;sudo apt upgrade -y
-
- cd "$HOME" || exit
- 
- flatpak install org.gnome.Platform/x86_64/3.38 -y
- 
- sleep 1s 
-
-git clone https://github.com/makrker/based.git
-
- echo eval "$(oh-my-posh --init --shell bash --config ~/based/asees.omp.json)" >> .bashrc
 
 sudo apt update -y;sudo apt upgrade -y
-
-flatpak remote-add --user --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
-
- cd "$HOME" || exit
-
-
-
-sudo apt update -y
-
-
-rm -rf .hyper.js
-cd based || exit 
-git pull
-mv hyper.js /home/"$USER"/.hyper.js
-sudo mv Firacode.ttf /usr/share/fonts/truetype
-cd || exit #
+ flatpak install flathub us.zoom.Zoom
+flatpak install flathub com.visualstudio.code-oss
 
 flatpak install flathub io.gitlab.librewolf-community -y
 
 flatpak install flathub com.github.wwmm.pulseeffects -y
 
-sudo apt update --fix-missing -y
+ 
+ flatpak install org.gnome.Platform/x86_64/3.38 -y
+ 
+
+sudo apt update -y;sudo apt upgrade -y
+
+
+ 
+
+sudo apt update -y;sudo apt upgrade -y
+ cd "$HOME" || exit
+git clone https://github.com/makrker/based.git
+
+sudo wget https://github.com/JanDeDobbeleer/oh-my-posh/releases/latest/download/posh-linux-amd64 -O /usr/local/bin/oh-my-posh
+sudo chmod +x /usr/local/bin/oh-my-posh
+ echo eval "$(oh-my-posh --init --shell bash --config ~/based/asees.omp.json)" >> .bashrc
+sudo apt update -y;sudo apt upgrade -
+sudo apt update -y
+
+
+sudo apt update --fix-broken -y
 sudo apt install -f -y
 
 sudo apt autoremove -y
-sudo apt install perl -y
-
-
 
 PS3='Please enter your choice: '
-options=("KDE Plasma" "Gnome" "Taco" "Quit")
+options=("KDE Plasma" "Gnome" "MATE" "Quit")
 select opt in "${options[@]}"
 do
     case $opt in
@@ -114,8 +120,10 @@ git clone https://github.com/vinceliuice/Orchis-theme.git
             gsettings set org.gnome.desktop.background picture-uri  file:///home/"$USER"/based/wallpaper.jpg
              break
             ;;
-        "Taco")
-            echo "Tacos Are Really, Really Old While there is some debate over when exactly the first taco was created, most experts state that the first taco was actually invented somewhere between 1,000 and 500 B.C. At the time, the taco was more about having an edible spoon and since has morphed into the dish that we know today."
+        "MATE")
+            
+            gsettings set org.mate.background picture-filename "$(find ~/based/wallpaper.jpg -type f | shuf -n1)"
+            break 
             ;;
         "Quit")
             break
@@ -126,7 +134,7 @@ done
 
 
 cd "$HOME" || exit
-cd Desktop || exit
+cd ∼/Desktop || exit
 wget https://addons.mozilla.org/firefox/downloads/file/3763728/dark_reader-4.9.32-an+fx.xpi
 wget https://addons.mozilla.org/firefox/downloads/file/3669496/trace-3.0.5-an+fx.xpi
 wget https://addons.cdn.mozilla.net/user-media/addons/839767/clearurls-1.21.0-an+fx.xpi?filehash=sha256%3Aeb0b199cd478b21bc1fab59be627603946d1474070aa4eb482c44d18c041d974
@@ -147,7 +155,7 @@ do
             sudo apt install gcc -y
             sudo apt install make -y
              git clone https://github.com/abba23/spotify-adblock-linux.git
-               cd spotify-adblock-linux || exit
+             cd spotify-adblock-linux || exit
               wget -O cef.tar.bz2 https://cef-builds.spotifycdn.com/cef_binary_88.1.6%2Bg4fe33a1%2Bchromium-88.0.4324.96_linux64_minimal.tar.bz2
              tar -xf cef.tar.bz2 --wildcards '*/include' --strip-components=1
                make
@@ -162,8 +170,8 @@ do
              *) echo "invalid option  stooped $REPLY";;
     esac
 done
-cd Desktop || exit
- touch Spotify
+cd ∼/Desktop || exit
+ touch Spotify.desktop
  echo "[Desktop Entry]
 Type=Application
 Name=Spotify (adblock)
@@ -174,10 +182,10 @@ Exec=env LD_PRELOAD=/usr/local/lib/spotify-adblock.so spotify %U
 Terminal=false
 MimeType=x-scheme-handler/spotify;
 Categories=Audio;Music;Player;AudioVideo;
-StartupWMClass=spotify" >> Spotify
+StartupWMClass=spotify" >> Spotify.desktop
 
 chmod +x Spotify
-
+cd #
 if [ ! -x /usr/bin/steam ]
 then echo "Steam is not installed, perform this?(y/n)"
     read -r ops
@@ -186,13 +194,25 @@ then echo "Steam is not installed, perform this?(y/n)"
            sudo dpkg -i steam_latest.deb
            
            then echo "Steam is installed"
-        else echo "unable to install the Steam. you are using sudo?" ; break
+        else echo "Something is wrong or broken exiting.." ; break
         fi ;;
      n) echo "Cancelled by $USER" ; break ;;
     esac
 fi
 
+if [ ! -x /usr/share/discord ]
+then echo "Discord is not installed, perform this?(y/n)"
+    read -r ops
+    case $ops in
+     y) if wget https://discord.com/api/download?platform=linux&format=deb
+         sudo dpkg -i download?platform=linux
 
+           then echo "Discord is installed"
+        else echo "Something is wrong or broken exiting.." ; break
+        fi ;;
+     n) echo "user requested no" ;break ;;
+    esac
+fi
 
 
 
@@ -201,16 +221,47 @@ then echo "Hyper is not installed, perform this?(y/n)"
     read -r ops
     case $ops in
      y) if wget get https://github.com/vercel/hyper/releases/download/3.1.0/hyper_3.1.0_amd64.deb
-            sudo dpkg -i hyper_3.0.2_amd64.deb
+            sudo dpkg -i hyper_3.1.0_amd64.deb
+            rm -rf .hyper.js
+            cd based || exit 
+            git pull
+            mv hyper.js /home/"$USER"/.hyper.js
+            sudo mv Firacode.ttf /usr/share/fonts/truetype
+
            then echo "Hyper is installed"
-        else echo "unable to install the hyper. you are using sudo?" ; break
+        else echo "Something is wrong or broken exiting.." ; break
         fi ;;
      n) echo "user requested no" ;break ;;
     esac
 fi
- 
+ sudo apt autoremove -y
 
 sudo apt --fix-broken install -y 
+
+
+echo "Do you like to remove all deb files?"
+PS3='Please enter your choice: '
+options=("Yes" "No")
+select opt in "${options[@]}"
+do
+    case $opt in
+        "Yes")
+            sudo rm -rf spotify-client_1.1.55.498.gf9a83c60_amd64
+                sudo rm -rf   hyper_3.1.0_amd64.deb
+               sudo rm -rf  download?platform=linux
+               sudo rm -rf   steam_latest.deb
+               
+               break
+              ;;
+	          "No")
+	          echo "User requested exit"
+	           break
+               	    ;;
+             *) echo "invalid option  stooped $REPLY";;
+    esac
+done
+
+
 
 
 figlet -f big "DONE INSTALLING PRECONFIGURED SHELLS
