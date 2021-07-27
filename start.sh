208edddc7885d0b8a075817a03e45d01567a9cd2
makrker <86200419+makrker@users.noreply.github.com>
2021-07-27 03:41:56 +0000

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,26 +1,12 @@
 #!/bin/bash 
 
 if [[ "$OSTYPE" == "linux-gnu"* ]]; then
-   if [[ $(command -v brew) == "" ]]; then
-    echo "You have not installed Homebrew "
+  
     
      sudo apt install wget -y
       cd || exit #
        wget https://raw.githubusercontent.com/makrker/vdfvd/main/main.sh
         chmod +x main.sh
-
-          ./main.sh
-       else
-         echo "You have installed Homebrew"
-           sudo apt install wget -y
-            cd /home/"$USER" || exit 
-            wget https://raw.githubusercontent.com/makrker/vdfvd/main/side.sh
-
-             chmod +x side.sh
-
-              ./side.sh
-
-
     fi
 fi
 
