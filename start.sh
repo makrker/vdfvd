f61302f6399ff443768fd332b2f74e9862b46030
makrker <86200419+makrker@users.noreply.github.com>
2021-07-28 08:57:39 -0500

    Update start.sh

--- start.sh
+++ start.sh
@@ -3,10 +3,21 @@
 if [[ "$OSTYPE" == "linux-gnu"* ]]; then
   
     
+if [ "$apt" == "" ]; then
+   echo "you have apt"
+  
      sudo apt install wget -y
       cd || exit #
        wget https://raw.githubusercontent.com/makrker/vdfvd/main/main.sh
         chmod +x main.sh
+      else 
+      
+     sudo apt install wget -y
+      cd || exit #
+       wget https://raw.githubusercontent.com/makrker/vdfvd/main/solus.sh
+        chmod +x solus.sh
+      
+        
     fi
 fi
 
